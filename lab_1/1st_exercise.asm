72058885ab5c7ba53ef1589144c1bd736b77b9b3
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-25 12:57:25 +0300

    Added the first exercise of the first lab
    Calculating F0 , backing up original values
    
    Calculating F1 , with the backed up values

--- lab_1/1st_exercise.asm
+++ lab_1/1st_exercise.asm
@@ -5,23 +5,52 @@
 .def C=r18
 .def D=r19
 .def TEMP=r20
-.cseg
-.org 0
+.def BACKUP_A = r21
+.def BACKUP_B = r22
+.def BACKUP_C = r23
+.def BACKUP_D = r24
+.def F1 = r25
 
 START: clr temp ;PORT B AS INPUT
-out DDRB,temp ;port b as input
-ser temp 
-out DDRC,temp ;port c as output
 
-Read: in temp,PINB ;read port b
-mov A,temp ;store in A
-lsr temp ;shift right
-mov B,temp ;store in B 
-lsr temp ;shift right
-mov C,temp ;store in C
-lsr temp
-mov D,temp ;store in D
+IN TEMP,PORTB
 
+MOV A,TEMP
+ANDI A,0x01
+MOV BACKUP_A,A
+
+MOV B,TEMP
+LSR B ; bit 1 goes to bit 0
+ANDI B,0x01 ; mask bit 0
+MOV BACKUP_B,B
+
+MOV C,TEMP 
+LSR C ; bit 2 goes to bit 1
+LSR C ; bit 2 goes to bit 0
+ANDI C,0x01 ; mask bit 0
+MOV BACKUP_C,C
+
+MOV D,TEMP
+LSR D ; bit 3 goes to bit 2
+LSR D ; bit 3 goes to bit 1
+LSR D ; bit 3 goes to bit 0
+ANDI D,0x01 ; mask bit 0
+MOV BACKUP_D,D
+
+;THIS IS THE CALCULATION OF F0
 and A,BN ; A AND B' : A = A AND B'
 and B,D ; B AND D : B = B AND D
 or A,B ; A OR B : A = A OR B
+
+;THIS IS THE CALCULATION OF F1 F1= (A’+C’) ∙ (B’+D)
+MOV F1, BACKUP_A
+COM F1 ; F1 = NOT A
+MOV TEMP, BACKUP_C
+COM TEMP ; TEMP = NOT C
+OR F1 , TEMP ; F1 = F1 OR TEMP = NOT A OR NOT C
+
+MOV TEMP, BACKUP_B
+COM TEMP ; TEMP = NOT B
+OR TEMP, BACKUP_D ; TEMP = NOT B OR D
+
+AND F1, TEMP ; F1 = F1 AND TEMP = (NOT A OR NOT C) AND (NOT B OR D)
