093bd1caa8380a028fca5d839e6b495cde1b2a71
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-05-10 13:52:11 +0300

    Added spaces

--- Exercises_2/exercise_4/4.asm
+++ Exercises_2/exercise_4/4.asm
@@ -1,6 +1,6 @@
 START:
 LDA 2000H ; Load dip switches' value
-MVI A,B ;Copy the value to B
+MOV A,B ;Copy the value to B
 
 A0_AND_B0:
 ANI 01H ; Mask the least significant bit A = A AND 00000001
@@ -8,7 +8,7 @@ MOV C,A ; Copy the result to C
 MOV A,B ; Copy the value of B to A
 ANI 02H ; Mask the second least significant bit A = A AND 00000010
 RRC ; Rotate the accumulator right to get this as LSB
-ANA C; A = A AND C
+ANA C ; A = A AND C
 MOV E,A ; Load the value of E to A | A0 AND B0 IS AT LSB
 
 A1_AND_B1:
@@ -18,7 +18,7 @@ MOV C,A ; Copy the result to C
 MOV A,B ; Copy the value of B to A
 ANI 08H ; Mask the fourth least significant bit A = A AND 00001000
 RRC ; Rotate the accumulator right to get this as 3RD LSB
-ANA C; A = 4TH AND C (3RD) |POS 3 
+ANA C ; A = 4TH AND C (3RD) |POS 3 
 RRC ; A1 AND B1 NOW AT 2ND LSB
 MOV D,A ; Load the value of A TO D | D = 4TH AND 3RD | X1 | POS 2
 
@@ -47,11 +47,11 @@ MOV A,B ; Copy the value of B to A
 ANI 80H ; Mask the eighth least significant bit A = A AND 10000000 (A IS NOW THE 8TH BIT)
 RRC ; Rotate the accumulator right to get this as 7TH LSB
 XRA C ; A = A XOR C (7TH XOR 8TH) | POS 7
-RRC; A3 XOR B3 NOW AT 6TH LSB
-RRC; A3 XOR B3 NOW AT 5TH LSB
-RRC; A3 XOR B3 NOW AT 4TH LSB
+RRC ; A3 XOR B3 NOW AT 6TH LSB
+RRC ; A3 XOR B3 NOW AT 5TH LSB
+RRC ; A3 XOR B3 NOW AT 4TH LSB
 MOV H,A ; Load the value of A to H | X3 | POS 4
-RRC; A3 XOR B3 NOW AT 3RD LSB
+RRC ; A3 XOR B3 NOW AT 3RD LSB
 
 A2B2_OR_A3B3:
 ORA L ; A = A OR L
@@ -60,7 +60,7 @@ MOV C,A ; Load the value of A to C | X2 | POS 3
 ; X0 X1 X2 X3 | E D C H
 
 SET_BITS_FOR_OUTPUT:
-MVI A ,00H ; Clear the accumulator
+MVI A,00H ; Clear the accumulator
 ORA E ; Set the 1st bit
 ORA D ; Set the 2nd bit
 ORA C ; Set the 3rd bit
@@ -69,4 +69,3 @@ CMA ; Invert the bits because of reverse logic
 STA 3000H ; Store the value to the output port
 
 END
-
