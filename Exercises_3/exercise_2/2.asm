04fdeb425d6b8a75c7d56cce8cfa1c0647d8f807
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-05-20 12:59:32 +0300

    Seems to work but is buggy sometimes

--- Exercises_3/exercise_2/2.asm
+++ Exercises_3/exercise_2/2.asm
@@ -1,74 +1,89 @@
-START:
-IN 10H
+START:	
+	IN 10H	
+	MVI A,0DH	
+	SIM				
+	EI	
+	
+	MVI B,06H
+	LXI H,0A00H	
+		
+FILL:
+	MVI M,10H		
+	INX H			
+	DCR B
+	JNZ FILL
 
-MVI A,10H  		 ;Set display
-STA 0B00H 
-STA 0B01H 
-STA 0B02H 
-STA 0B03H 
-STA 0B04H 
-STA 0B05H	
+	MVI C,63H ; 99
+	MVI D,80H ; 128
+	MVI E,C8H ;200
 
-MVI A,0DH			;Initialize interrupt mask
-SIM
-EI
-
-WAIT:
-	JMP WAIT
-INTR_ROUTINE:
-	POP H			;POP return address so that the stack doesn't fill up
-	EI				;Enable interrupts inside interrupt routine
-	MVI A,00H 		;Turn on LEDs
-	STA 3000H
-	MVI H,06H 		;Counter for 6 iterations
-	MOV A,H
-	DCR A			;Set up tens
-	STA 0B03H			;Store tens in the 4th segment display 
-SECONDS:
-	MVI A,09H			;Set up 9 secs (units)
-LIGHTS_ON:
-	STA 0B02H			;Store units in the 3rd segment display
-	CALL DISPLAY		
-	DCR A 			
-	CPI 00H 			;Compare with zero
-	JNZ LIGHTS_ON	 	;If Z=0 then 9 seconds passed
-	CALL ZERO			;Display zero unit (1 sec)
-	DCR H			;Decrease counter
-	JZ EXIT			;If Z=0 end timer
-	MOV A,H
-	DCR A		
-	STA 0B03H
-	JMP SECONDS		;Reapeat for 60 seconds
-EXIT:
-	MVI A,FFH 		;Turn off LEDs
-	STA 3000H 
-	JMP WAIT			;Return to wait (main program)
-DISPLAY: 	
-	LXI B,0064H 		;100 msec delay
-	LXI D,0B00H		;For STDM
-	PUSH PSW
-	PUSH H
 	PUSH D
-	PUSH B
+	LXI D,0A00H
 	CALL STDM
-	MVI A,0AH 		;10*100msec=1sec
-1SEC:
-	CALL DCD			
-	CALL DELB
-	DCR A
-	CPI 00H
-	JNZ 1SEC
-	POP B
 	POP D
-	POP H
-	POP PSW
-	RET
-ZERO: 				;Display zero in the 3rd segment display
-	MVI A,00H
-	STA 0B02H
-	CALL DISPLAY
-	CALL DELB
-	RET
+	CALL DCD
+
+Main_Loop:	
+	JMP Main_Loop
+
+INTR_ROUTINE:
+	DI
+	CALL KIND
+	STA 0A02H
+	MOV B,A
+	CALL KIND
+	STA 0A03H	
+	RLC
+	RLC
+	RLC
+	RLC
+	ADD B
 	
-	END
+	CMP C
+	JC First_threshold
+	JZ First_threshold
+	JMP Check_second_threshold
+
+First_threshold:	
+	MVI A,01H
+	JMP FINAL	
+
+Check_second_threshold:
+	CMP D
+	JC Second_threshold
+	JZ Second_threshold
+	JMP Check_third_threshold
+
+Second_threshold:
+	MVI A,02H
+	JMP FINAL
+
+Check_third_threshold:
+	CMP E
+	JC Third_threshold
+	JZ Third_threshold
+	JMP LED_4
+
+Third_threshold:
+	MVI A,04H
+	JMP FINAL
+
+LED_4:
+	MVI A,08H
+	JMP FINAL
+
+FINAL:
+	CMA
+	STA 3000H
+
+	PUSH D
+	LXI D,0A00H
+	CALL STDM
+	POP D
+	EI
 	
+DISP:
+	CALL DCD
+	JMP DISP
+
+END
